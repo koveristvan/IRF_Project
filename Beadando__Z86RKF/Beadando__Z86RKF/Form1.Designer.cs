ab760211864b8d2c894cd645cb3a466071e3b3ba
István Kövér <istvan.kover@stud.uni-corvinus.hu>
2020-12-14 12:32:42 +0100

    Átmásolva az IRF-projectbe

---
+++ Beadando__Z86RKF/Beadando__Z86RKF/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Beadando__Z86RKF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saveall = new System.Windows.Forms.Button();
+            this.searchbutton = new System.Windows.Forms.Button();
+            this.copymail = new System.Windows.Forms.Button();
+            this.rebindingsource = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.drawingpanel = new System.Windows.Forms.Panel();
+            this.entityCommand1 = new System.Data.Entity.Core.EntityClient.EntityCommand();
+            this.LocationTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SquareTextBox = new System.Windows.Forms.TextBox();
+            this.LevelsTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DirectionTextBox = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.search_textbox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rebindingsource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(556, 368);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // saveall
+            // 
+            this.saveall.Location = new System.Drawing.Point(611, 245);
+            this.saveall.Name = "saveall";
+            this.saveall.Size = new System.Drawing.Size(137, 56);
+            this.saveall.TabIndex = 2;
+            this.saveall.Text = "Export list in excel";
+            this.saveall.UseVisualStyleBackColor = true;
+            this.saveall.Click += new System.EventHandler(this.saveall_Click);
+            // 
+            // searchbutton
+            // 
+            this.searchbutton.Location = new System.Drawing.Point(611, 307);
+            this.searchbutton.Name = "searchbutton";
+            this.searchbutton.Size = new System.Drawing.Size(137, 56);
+            this.searchbutton.TabIndex = 3;
+            this.searchbutton.Text = "Search location in chrome";
+            this.searchbutton.UseVisualStyleBackColor = true;
+            this.searchbutton.Click += new System.EventHandler(this.searchbutton_Click_1);
+            // 
+            // copymail
+            // 
+            this.copymail.Location = new System.Drawing.Point(611, 369);
+            this.copymail.Name = "copymail";
+            this.copymail.Size = new System.Drawing.Size(137, 45);
+            this.copymail.TabIndex = 4;
+            this.copymail.Text = "Copy e-mail";
+            this.copymail.UseVisualStyleBackColor = true;
+            this.copymail.Click += new System.EventHandler(this.copymail_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Real Estate List Pest county";
+            // 
+            // drawingpanel
+            // 
+            this.drawingpanel.BackColor = System.Drawing.Color.Snow;
+            this.drawingpanel.Location = new System.Drawing.Point(790, 46);
+            this.drawingpanel.Name = "drawingpanel";
+            this.drawingpanel.Size = new System.Drawing.Size(471, 368);
+            this.drawingpanel.TabIndex = 6;
+            // 
+            // entityCommand1
+            // 
+            this.entityCommand1.CommandTimeout = 0;
+            this.entityCommand1.CommandTree = null;
+            this.entityCommand1.Connection = null;
+            this.entityCommand1.EnablePlanCaching = true;
+            this.entityCommand1.Transaction = null;
+            // 
+            // LocationTextbox
+            // 
+            this.LocationTextbox.Location = new System.Drawing.Point(103, 430);
+            this.LocationTextbox.Name = "LocationTextbox";
+            this.LocationTextbox.Size = new System.Drawing.Size(465, 26);
+            this.LocationTextbox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 430);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Location";
+            // 
+            // SquareTextBox
+            // 
+            this.SquareTextBox.Location = new System.Drawing.Point(103, 476);
+            this.SquareTextBox.Name = "SquareTextBox";
+            this.SquareTextBox.Size = new System.Drawing.Size(60, 26);
+            this.SquareTextBox.TabIndex = 9;
+            // 
+            // LevelsTextBox
+            // 
+            this.LevelsTextBox.Location = new System.Drawing.Point(301, 476);
+            this.LevelsTextBox.Name = "LevelsTextBox";
+            this.LevelsTextBox.Size = new System.Drawing.Size(41, 26);
+            this.LevelsTextBox.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 479);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "m^2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(169, 479);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Number of levels";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(348, 479);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Direction";
+            // 
+            // DirectionTextBox
+            // 
+            this.DirectionTextBox.Location = new System.Drawing.Point(426, 476);
+            this.DirectionTextBox.Name = "DirectionTextBox";
+            this.DirectionTextBox.Size = new System.Drawing.Size(142, 26);
+            this.DirectionTextBox.TabIndex = 14;
+            // 
+            // search_textbox
+            // 
+            this.search_textbox.Location = new System.Drawing.Point(1120, 463);
+            this.search_textbox.Name = "search_textbox";
+            this.search_textbox.Size = new System.Drawing.Size(141, 26);
+            this.search_textbox.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(800, 463);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(204, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Number of google searches";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 545);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.search_textbox);
+            this.Controls.Add(this.DirectionTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LevelsTextBox);
+            this.Controls.Add(this.SquareTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LocationTextbox);
+            this.Controls.Add(this.drawingpanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.copymail);
+            this.Controls.Add(this.searchbutton);
+            this.Controls.Add(this.saveall);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rebindingsource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button saveall;
+        private System.Windows.Forms.Button searchbutton;
+        private System.Windows.Forms.Button copymail;
+        private System.Windows.Forms.BindingSource rebindingsource;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.Entity.Core.EntityClient.EntityCommand entityCommand1;
+        public System.Windows.Forms.Panel drawingpanel;
+        private System.Windows.Forms.TextBox LocationTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox SquareTextBox;
+        private System.Windows.Forms.TextBox LevelsTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox DirectionTextBox;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox search_textbox;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Beadando__Z86RKF/Beadando__Z86RKF/Form1.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Beadando__Z86RKF
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+        ReaEstateEntities context = new ReaEstateEntities();
+        List<Realestate> realestates;
+
+        
+       
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            context.Realestate.Load();
+            rebindingsource.DataSource = context.Realestate.Local;
+            dataGridView1.DataSource = rebindingsource;
+            Value = 0;
+            
+    }
+        
+
+        public void LoadData()
+        {
+            realestates = context.Realestate.ToList();
+           
+        }
+
+        private void saveall_Click(object sender, EventArgs e)
+        {
+            createexcel();
+        }
+
+        public void createexcel()
+        {
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        public void CreateTable()
+        {
+            string[] headers = new string[]
+                {
+                     "Cím",
+                     "Négyzetméter",
+                     "Szín",
+                     "Fekvés",
+                     "Szintek száma",
+                     "Medence",
+                     "E-mail",
+                };
+            xlSheet.Cells[1, 1] = headers[0];
+            xlSheet.Cells[1, 2] = headers[1];
+            xlSheet.Cells[1, 3] = headers[2];
+            xlSheet.Cells[1, 4] = headers[3];
+            xlSheet.Cells[1, 5] = headers[4];
+            xlSheet.Cells[1, 6] = headers[5];
+            xlSheet.Cells[1, 7] = headers[6];
+            object[,] values = new object[realestates.Count, headers.Length];
+
+
+            int counter = 0;
+            foreach (Realestate re in realestates)
+            {
+                values[counter, 0] = re.LOCATION;
+                values[counter, 1] = re.SQUAREM;
+                values[counter, 2] = re.COLOR;
+                values[counter, 3] = re.DIRECTION;
+                values[counter, 4] = re.LEVELS;
+                if (re.POOL==true)
+                {
+                    values[counter, 5] = "Van";
+                }
+                else
+                {
+                    values[counter, 5] = "Nincs";
+                }
+                
+                values[counter, 6] =re.EMAIL ;
+                counter++;
+            }
+
+           string GetCell(int x, int y)
+            {
+                string ExcelCoordinate = "";
+                int dividend = y;
+                int modulo;
+
+                while (dividend > 0)
+                {
+                    modulo = (dividend - 1) % 26;
+                    ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                    dividend = (int)((dividend - modulo) / 26);
+                }
+                ExcelCoordinate += x.ToString();
+
+                return ExcelCoordinate;
+            }
+
+            xlSheet.get_Range(
+                         GetCell(2, 1),
+                         GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 50;
+            headerRange.Interior.Color = Color.DeepSkyBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+        }
+
+
+
+
+        public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+            DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+            string Location = selectedRow.Cells["LOCATION"].Value.ToString();
+            int Levels = int.Parse(selectedRow.Cells["LEVELS"].Value.ToString());
+            string Colour = Convert.ToString(selectedRow.Cells["COLOR"].Value);
+            float Squarem = float.Parse(selectedRow.Cells["SQUAREM"].Value.ToString());
+            string Direction = Convert.ToString(selectedRow.Cells["DIRECTION"].Value);
+            bool Pool = bool.Parse(selectedRow.Cells["POOL"].Value.ToString());
+            float multiple = 2;
+            int width = int.Parse((Squarem * multiple).ToString());
+
+            
+            drawingpanel.Refresh();
+            using (Graphics g = drawingpanel.CreateGraphics())
+            {
+
+                Pen mypen = new Pen(Color.Black);
+                Pen mypen2 = new Pen(Color.Black);
+                SolidBrush mybrush = new SolidBrush(Color.Black);
+                SolidBrush mybrush2 = new SolidBrush(Color.Brown);
+                SolidBrush windowbrush = new SolidBrush(Color.DarkGray);
+                if (dataGridView1.SelectedCells.Count > 0)
+                {
+                    //változók beolvasása a datagridviewból
+
+
+                    LocationTextbox.Text = Location;
+                    SquareTextBox.Text = Squarem.ToString();
+                    DirectionTextBox.Text = Direction;
+                    LevelsTextBox.Text = Levels.ToString();
+
+                    // házszín
+                    switch (Colour)
+                    {
+                        case "Red":
+                            mybrush.Color = Color.Red;
+                            break;
+                        case "Blue":
+                            mybrush.Color = Color.Blue;
+                            break;
+                        case "Green":
+                            mybrush.Color = Color.Green;
+                            break;
+                        case "Yellow":
+                            mybrush.Color = Color.Yellow;
+                            break;
+
+                        default:
+                            mybrush.Color = Color.White;
+                            MessageBox.Show("A ház igazi színe nincs a rendszerben");
+                            break;
+                    }
+                    //egyszintes ház
+                    if (Levels == 1)
+                    {
+                        g.DrawRectangle(mypen, 50, 187, width, 50);
+                        g.FillRectangle(mybrush, 50 + 1, 187 + 1, width - 1, 50 - 1);
+
+                        Point point1 = new Point(50, 187);
+                        Point point2 = new Point((50 + (width) / 2), 150);
+                        Point point3 = new Point(50 + width, 187);
+                        Point[] trianglepoints = { point1, point2, point3 };
+                        g.FillPolygon(mybrush2, trianglepoints); //tető
+                        g.FillRectangle(windowbrush, 70, 200, 20, 20);
+
+                    }
+                    //többszintes ház
+                    if (Levels != 1)
+                    {
+                        for (int i = 0; i < Levels; i++)
+                        {
+                            g.DrawRectangle(mypen, 50, 187 - (50 * i), width / Levels, 50);
+                            g.FillRectangle(mybrush, 50 + 1, 187 - (50 * i) + 1, width / Levels - 1 * Levels / multiple, 50 - 1);
+                            if (Squarem / Levels > 50)
+                            {
+                                g.FillRectangle(windowbrush, 70, 200 - (i * 50), 20, 20);
+                            }
+
+                            if (i > 0 & Squarem > 100)
+                            {
+                                g.FillRectangle(windowbrush, width / Levels + 10, 200 - (i * 50), 20, 20);
+                            }
+                        }
+
+                    }
+                    //medence
+                    if (Pool == true)
+                    {
+                        g.DrawLine(mypen2, 50 + width / Levels + 20, 237, 50 + width / Levels + 70, 237);
+                        g.DrawLine(mypen2, 50 + width / Levels + 20, 237, 50 + width / Levels + 20, 207);
+                        g.DrawLine(mypen2, 50 + width / Levels + 70, 237, 50 + width / Levels + 70, 207);
+                        g.DrawEllipse(mypen2, 50 + width / Levels + 20, 197, 50, 20);
+                        g.FillEllipse(new SolidBrush(Color.Blue), 50 + width / Levels + 20, 197, 50, 20);
+                    }
+                    g.FillRectangle(mybrush2, width / Levels, 200, 20, 37); //ajtó
+
+
+                }
+
+            }
+
+        }
+
+
+
+        private void copymail_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                
+                    int selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+                    DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+                    string mail = selectedRow.Cells["EMAIL"].Value.ToString();
+
+                    Clipboard.SetText(mail);
+                
+            }
+            
+        }
+
+
+        private int _value;
+
+        public int Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+
+            }
+        }
+        private void searchbutton_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                if (Value < 5)
+                {
+
+                    string URL = LocationTextbox.Text;
+
+                    Process.Start("https://www.google.be/search?q=" + URL);
+
+                    Value++;
+                    search_textbox.Text = Value.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("You are using the search function too much");
+                }
+            }
+        }
+
+       
+    }
+}
